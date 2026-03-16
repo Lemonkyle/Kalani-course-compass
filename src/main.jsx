cea9a3d734c75281d2504df9f18edc6af3e4f2d9
Lemonkyle <kyleexcalibur58@gmail.com>
2026-03-15 15:58:56 -1000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../kalani-planner.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AdminApp from "./admin/AdminApp.jsx";
+import KalaniPlanner from "../kalani-planner.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = createRoot(document.getElementById("root"));
+
+// Route /admin to admin panel, everything else to the student planner
+if (window.location.pathname.startsWith("/admin")) {
+  root.render(<StrictMode><AdminApp /></StrictMode>);
+} else {
+  root.render(<StrictMode><KalaniPlanner /></StrictMode>);
+}
